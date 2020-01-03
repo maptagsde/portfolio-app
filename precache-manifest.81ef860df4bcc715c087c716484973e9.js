7a394cbc3f0948817e57eb9c893ebdce06d5a042
Simon Appelt <simon.appelt@finanzcheck.de>
2020-01-03 16:09:40 +0100

    Updates

---
+++ precache-manifest.81ef860df4bcc715c087c716484973e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d6c2b843e9963871776c4e707877dd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3acb6da91455ee337aaa",
+    "url": "/portfolio/static/css/main.3f314210.chunk.css"
+  },
+  {
+    "revision": "faf90623bd768605de2c",
+    "url": "/portfolio/static/js/2.0a4a0535.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/portfolio/static/js/2.0a4a0535.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3acb6da91455ee337aaa",
+    "url": "/portfolio/static/js/main.08d8ac03.chunk.js"
+  },
+  {
+    "revision": "8575fab3f3395c90d845",
+    "url": "/portfolio/static/js/runtime-main.680679b4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/index.html/precache-manifest.07d3ae82c4ba342954e6bae53d850ca2.js"
+  "/portfolio/precache-manifest.81ef860df4bcc715c087c716484973e9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
